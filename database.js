ffd3901e8ef1c950859261a4269731cca89febc9
2000jedi <misakamikotojedi@icloud.com>
2017-04-30 17:14:47 +0800

    Initial Post
    Finish main structure and overview

---
+++ database.js
@@ -0,0 +1,85 @@
+/**
+ * Created by 2000jedi on 2017/4/30.
+ */
+
+const fs = require("fs");
+const md = require("markdown").markdown;
+module.exports = {
+    getNotebooks: function () {
+        const lib = getLibPath();
+        fs.readdir(lib, function (err, files) {
+            if (err) {
+                console.log("Error reading library");
+                return
+            }
+
+            files.forEach(function (file) {
+                if (file.toString() !== "Trash.qvnotebook") {
+                    let elem = document.createElement("div");
+                    elem.innerHTML = file.toString().split('.')[0];
+                    elem.setAttribute("onclick","db.getNotes('" + elem.innerHTML + "');");
+                    document.getElementById("menu").appendChild(elem);
+                }
+            });
+        });
+    },
+
+    getNotes: function (notebook_name) {
+        cur_notebook = notebook_name;
+        const lib = getLibPath() + "/" + notebook_name + ".qvnotebook";
+        fs.readdir(lib, function (err, files) {
+            if (err) {
+                console.log("Error reading notebook");
+                return
+            }
+            document.getElementById("submenu").innerHTML = "";
+            files.forEach(function (file) {
+                if (file.toString() !== "meta.json") {
+                    let elem = document.createElement("div");
+                    let file_description = JSON.parse(fs.readFileSync(lib+"/"+file.toString()+"/meta.json", "utf8"));
+                    elem.innerHTML = file_description.title;
+                    elem.setAttribute("onclick", "db.getNote('" + notebook_name + "', '" + file.toString() + "');");
+                    document.getElementById("submenu").appendChild(elem);
+                }
+            });
+        });
+    },
+
+    getNote: function (notebook_name, note_dir) {
+        cur_notebook = notebook_name;
+        cur_note = note_dir;
+        const lib = getLibPath() + '/' + notebook_name + '.qvnotebook/' + note_dir + '/';
+        let meta = JSON.parse(fs.readFileSync(lib + 'meta.json', 'utf8'));
+        let content = JSON.parse(fs.readFileSync(lib + 'content.json', 'utf8'));
+        document.getElementById("content").innerHTML = "";
+        content.cells.forEach(function(cell){
+            let elem = document.createElement("div");
+            if (cell.type === "markdown"){
+                elem.innerHTML = md.toHTML(content.cells[0].data);
+            } else {
+                elem.innerHTML = content.cells[0].data;
+            }
+            document.getElementById("content").appendChild(elem);
+        });
+
+    },
+
+    updateNote: function (notebook_name, note_name, content) {
+        const lib = getLibPath();
+    },
+
+    newNote: function (notebook_name, note_name) {
+        const lib = getLibPath();
+    },
+
+    newNoteBook: function (notebook_name) {
+        const lib = getLibPath();
+    },
+
+    parseCell: function (content) {
+    }
+};
+
+function getLibPath(){
+    return "D:\\Documents\\记录\\Quiver.qvlibrary";
+}
---
+++ main.js
@@ -0,0 +1,32 @@
+/**
+ * Created by 2000jedi on 2017/4/30.
+ */
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow(){
+    mainWindow=new BrowserWindow({
+        width:1280,
+        height:800,
+        resizable: false
+    });
+    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+    mainWindow.on("closed",function(){
+        mainWindow = null;
+    });
+}
+
+app.on("ready",createWindow);
+app.on("window-all-closed",function(){
+    if(process.platform!=="darwin"){
+        app.quit();
+    }
+});
+app.on("activate",function(){
+    if(mainWindow===null){
+        createWindow();
+    }
+});
